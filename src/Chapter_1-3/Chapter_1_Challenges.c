8c72a376481b0e81e509208630772a82bc45b793
Makinzi Nagashima <Makinzijc@gmail.com>
2026-02-05 00:00:49 -0800

    Add a search function

--- src/Chapter_1-3/Chapter_1_Challenges.c
+++ src/Chapter_1-3/Chapter_1_Challenges.c
@@ -1,7 +1,191 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct Node {
+    int data;
+    struct Node* next;
+    struct Node* prev;
+} Node;
+
+Node* createNode(int data)
+{
+    Node* newNode = malloc(sizeof(Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    newNode->prev = NULL;
+    return newNode;
+}
+
+void insertAtBeginning(Node** head, int data)
+{
+    Node* newNode = createNode(data);
+    if (*head == NULL) {
+        *head = newNode;
+        return;
+    }
+    newNode->next = *head;
+    (*head)->prev = newNode;
+    *head = newNode;
+}
+
+void insertAtEnd(Node** head, int data)
+{
+    Node* newNode = createNode(data);
+    if (*head == NULL) {
+        *head = newNode;
+        return;
+    }
+
+    Node* temp = *head;
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+    newNode->prev = temp;
+}
+
+void insertAtPosition(Node** head, int data, int position)
+{
+    if (position < 1) {
+        printf("Position needs to be >= 1.\n");
+        return;
+    }
+
+    if (position == 1) {
+        insertAtBeginning(head, data);
+        return;
+    }
+    Node* newNode = createNode(data);
+    Node* temp = *head;
+    for (int i = 1; temp != NULL && i < position - 1; i++) {
+        temp = temp->next;
+    }
+    if (temp == NULL) {
+        printf("Position is larger than the number of existing nodes\n");
+        free(newNode);
+        return;
+    }
+    newNode->next = temp->next;
+    newNode->prev = temp;
+    if (temp->next != NULL) {
+        temp->next->prev = newNode;
+    }
+    temp->next = newNode;
+}
+
+void deleteAtBeginning(Node** head)
+{
+    if (*head == NULL) {
+        printf("The list is empty.\n");
+        return;
+    }
+    Node* temp = *head;
+    *head = (*head)->next;
+    if (*head != NULL) {
+        (*head)->prev = NULL;
+    }
+    free(temp);
+}
+
+void deleteAtEnd(Node** head)
+{
+    if (*head == NULL) {
+        printf("The list is empty.\n");
+        return;
+    }
+    Node* temp = *head;
+    if (temp->next == NULL) {
+        *head = NULL;
+        free(temp);
+        return;
+    }
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+    temp->prev->next = NULL;
+    free(temp);
+}
+
+void deleteAtPosition(Node** head, int position)
+{
+    if (*head == NULL) {
+        printf("The list is empty.\n");
+        return;
+    }
+    Node* temp = *head;
+    if (position == 1) {
+        deleteAtBeginning(head);
+        return;
+    }
+    for (int i = 1; temp != NULL && i < position; i++) {
+        temp = temp->next;
+    }
+    if (temp == NULL) {
+        printf("Position is larger than the number of existing nodes\n");
+        return;
+    }
+    if (temp->next != NULL) {
+        temp->next->prev = temp->prev;
+    }
+    if (temp->prev != NULL) {
+        temp->prev->next = temp->next;
+    }
+    free(temp);
+}
+
+void search(Node* head, int key)
+{
+    if (head == NULL) {
+        printf("The list is empty.\n");
+        return;
+    }
+
+    Node* temp = head;
+    int position = 1;
+
+    while (temp != NULL) {
+        if (temp->data == key) {
+            printf("%d found at position %d.\n", key, position);
+            return;
+        }
+        temp = temp->next;
+        position++;
+    }
+
+    printf("%d not found in the list.\n", key);
+}
+
+void printList(Node* head)
+{
+    Node* temp = head;
+    printf("List: ");
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+    printf("\n");
+}
+
 int main()
 {
     printf("Hello World\n");
+
+    Node* head = NULL;
+
+    insertAtEnd(&head, 1);
+    insertAtEnd(&head, 2);
+    insertAtBeginning(&head, 0);
+    insertAtPosition(&head, 3, 2);
+    search(head, 3);
+    printf("After Insertions:\n");
+    printList(head);
+    deleteAtBeginning(&head);
+    deleteAtEnd(&head);
+    deleteAtPosition(&head, 2);
+    printf("After Deletions:\n");
+    search(head, 3);
+    search(head, 1);
+    printList(head);
+
+    return 0;
 }

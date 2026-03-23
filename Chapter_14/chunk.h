4c55bc79ef2ab4f54ba36de87bdb24f1974a9781
Makinzi Nagashima <Makinzijc@gmail.com>
2026-03-22 19:24:38 -0700

    Add code for Chapters 14 and 15

---
+++ Chapter_14/chunk.h
@@ -0,0 +1,35 @@
+#ifndef clox_chunk_h
+#define clox_chunk_h
+
+#include "common.h"
+#include "value.h"
+
+typedef enum {
+  OP_CONSTANT,
+  OP_CONSTANT_LONG,
+  OP_RETURN,
+} OpCode;
+
+typedef struct {
+  int offset;
+  int line;
+} LineStart;
+
+typedef struct {
+  int count;
+  int capacity;
+  uint8_t* code;
+  ValueArray constants;
+  int lineCount;
+  int lineCapacity;
+  LineStart* lines;
+} Chunk;
+
+void initChunk(Chunk* chunk);
+void freeChunk(Chunk* chunk);
+void writeChunk(Chunk* chunk, uint8_t byte, int line);
+void writeConstant(Chunk* chunk, Value value, int line);
+int addConstant(Chunk* chunk, Value value);
+int getLine(Chunk* chunk, int instruction);
+
+#endif
---
+++ Chapter_15/vm.c
@@ -0,0 +1,103 @@
+#include <stdio.h>
+
+#include "common.h"
+#include "debug.h"
+#include "memory.h"
+#include "vm.h"
+
+VM vm; // [one]
+
+static void resetStack() {
+  vm.stackCount = 0;
+}
+
+void initVM() {
+  vm.stack = NULL;
+  vm.stackCapacity = 0;
+  resetStack();
+}
+
+void freeVM() {
+  FREE_ARRAY(Value, vm.stack, vm.stackCapacity);
+  vm.stack = NULL;
+  vm.stackCapacity = 0;
+  resetStack();
+}
+
+void push(Value value) {
+  if (vm.stackCapacity < vm.stackCount + 1) {
+    int oldCapacity = vm.stackCapacity;
+    vm.stackCapacity = GROW_CAPACITY(oldCapacity);
+    vm.stack = GROW_ARRAY(Value, vm.stack, oldCapacity,
+        vm.stackCapacity);
+  }
+
+  vm.stack[vm.stackCount] = value;
+  vm.stackCount++;
+}
+
+Value pop() {
+  vm.stackCount--;
+  return vm.stack[vm.stackCount];
+}
+
+static InterpretResult run() {
+#define READ_BYTE() (*vm.ip++)
+#define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define BINARY_OP(op) \
+    do { \
+      double b = pop(); \
+      double a = pop(); \
+      push(a op b); \
+    } while (false)
+
+  for (;;) {
+#ifdef DEBUG_TRACE_EXECUTION
+    printf("          ");
+    for (Value* slot = vm.stack; slot < vm.stack + vm.stackCount; slot++) {
+      printf("[ ");
+      printValue(*slot);
+      printf(" ]");
+    }
+    printf("\n");
+    disassembleInstruction(vm.chunk,
+                           (int)(vm.ip - vm.chunk->code));
+#endif
+
+    uint8_t instruction;
+    switch (instruction = READ_BYTE()) {
+      case OP_CONSTANT: {
+        Value constant = READ_CONSTANT();
+        push(constant);
+        break;
+      }
+      case OP_ADD:      BINARY_OP(+); break;
+      case OP_SUBTRACT: BINARY_OP(-); break;
+      case OP_MULTIPLY: BINARY_OP(*); break;
+      case OP_DIVIDE:   BINARY_OP(/); break;
+      case OP_NEGATE:   push(-pop()); break;
+      case OP_RETURN: {
+        printValue(pop());
+        printf("\n");
+        return INTERPRET_OK;
+      }
+    }
+  }
+
+#undef READ_BYTE
+#undef READ_CONSTANT
+#undef BINARY_OP
+}
+
+void hack(bool b) {
+  // Hack to avoid unused function error. run() is not used in the
+  // scanning chapter.
+  run();
+  if (b) hack(false);
+}
+
+InterpretResult interpret(Chunk* chunk) {
+  vm.chunk = chunk;
+  vm.ip = vm.chunk->code;
+  return run();
+}
